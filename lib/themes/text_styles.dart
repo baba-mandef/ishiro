6c36a4ae182f5302447b1a4b1dc3d11f723b54e2
baba-mandef <contact@abiodoun.dev>
2024-03-23 16:50:10 +0100

    📝 add text style to theme

---
+++ lib/themes/text_styles.dart
@@ -0,0 +1,4 @@
+import 'package:flutter/material.dart';
+import './colors.dart';
+
+TextStyle splashBrandText = TextStyle(color: brandBlue, fontFamily: "ReemKufiInk", fontSize: 24.0,  fontWeight: FontWeight.w500,);
